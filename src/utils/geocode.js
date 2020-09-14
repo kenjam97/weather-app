98031a7d6647877681ecfc7872003c8196bd75e9
James Kennedy <jam.ken1997@gmail.com>
2020-09-14 01:11:40 +0100

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('postman-request')
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address,
-  )}.json?access_token=pk.eyJ1Ijoia2VuamFtOTciLCJhIjoiY2tldTB2YTNnMGE1MjJ6bWVtem43Zmg3aiJ9.FeqWGV-ewIC-UCjN1Ve5Fw&limit=1`
+  )}.json?access_token=KEY&limit=1`
 
   request(url, { json: true }, (error, response) => {
     const data = response.body.features
